0ca5598b0af93ee8b33309587eefaabbcbe9caa9
Jakob <jakob@sbusk.dk>
2020-03-20 01:02:38 +0100

    master

---
+++ musikbottenbuskv-1.0/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Our main entrypoint to run the Musicbot
+#
+
+# Errors are fatal
+set -e
+
+# Debugging
+#set -x
+
+cd /var/Musicbot/
+
+pushd config/ 2>/dev/null
+
+cat options.ini.IN | sed \
+	-e "s/%TOKEN%/$TOKEN/" \
+	-e "s/%OWNER_ID%/$OWNER_ID/" \
+	-e "s/%TEXT_CHANNEL%/$TEXT_CHANNEL/" \
+	-e "s/%VOICE_CHANNEL%/$VOICE_CHANNEL/" \
+	> options.ini
+
+if test "$GRANT_TO_ROLES"
+then
+	cat permissions.ini.IN |sed -e "s/;%GRANT_TO_ROLES%/GrantToRoles = $GRANT_TO_ROLES/" > permissions.ini
+
+else
+	echo "# "
+	echo "# Variable GRANT_TO_ROLES not specified, not parsing file but just copying it over."
+	echo "# "
+	cp permissions.ini.IN permissions.ini
+
+fi
+
+#
+# Delete comments and write the output to the autoplaylist.txt file
+# 
+cat autoplaylist.txt.IN | sed -e "/^#/d" > autoplaylist.txt
+
+popd 2>/dev/null
+
+/usr/bin/python3 /var/Musicbot/run.py
