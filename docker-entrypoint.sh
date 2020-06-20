50a9947ac1aba2ccfc7c5f4e6c638145ea0f6d01
Jakob <jakob@sbusk.dk>
2020-06-20 02:49:48 +0200

    Can now use env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,11 @@ if [[ ! -f "/musicbot/config/example_options.ini" ]]; then
     cp -r /musicbot/sample_config/* /musicbot/config
 fi
 
+    cat /musicbot/config/example_options.ini | sed \
+	-e "s/%TOKEN%/$TOKEN/" \
+	-e "s/%SPOTIFY_ID%/$SPOTIFY_ID/" \
+	-e "s/%SPOTIFY_SECRET%/$SPOTIFY_SECRET/" \
+	-e "s/%PREFIX%/$PREFIX/" \
+	> /musicbot/config/options.ini
+
 exec python3 run.py $@
